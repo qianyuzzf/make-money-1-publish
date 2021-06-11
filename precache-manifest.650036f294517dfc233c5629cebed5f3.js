3986fae14a5991109e3d3f892519466cfe653bdb
qianyu <280624745@qq.com>
2021-06-11 20:29:13 +0800

    deploy

---
+++ precache-manifest.650036f294517dfc233c5629cebed5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0a001b32112be77e90",
+    "url": "/make-money-1-publish/css/app.be17673b.css"
+  },
+  {
+    "revision": "e0a53bdac9aca36e19c2c22dac79bc96",
+    "url": "/make-money-1-publish/index.html"
+  },
+  {
+    "revision": "7f0a001b32112be77e90",
+    "url": "/make-money-1-publish/js/app.a6b6842e.js"
+  },
+  {
+    "revision": "a07a01e0d8e25b3d9ae3",
+    "url": "/make-money-1-publish/js/chunk-vendors.a2b0e00c.js"
+  },
+  {
+    "revision": "86c0dd2648f62741d62b58fa5963f518",
+    "url": "/make-money-1-publish/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/make-money-1-publish/robots.txt"
+  }
+]);
